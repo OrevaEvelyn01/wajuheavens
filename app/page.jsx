1f3ea19fbf5e1cb0e857ed09128cd76f4f773c6f
Obison Orevaoghene Chiamaka <orevaevelyn1@gmail.com>
2025-08-31 18:20:39 +0100

    changes

--- app/page.jsx
+++ app/page.jsx
@@ -4,8 +4,43 @@ import React, { useState, useEffect } from "react";
 import { FaChevronLeft } from "react-icons/fa6";
 import { FaChevronRight } from "react-icons/fa6";
 import Link from "next/link";
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "@/lib/firebase.config";
 
 export default function Page() {
+  const [events, setEvents] = useState([]);
+
+  const fetchEvents = async () => {
+
+    let reviewArray = []
+    const querySnapshot = await getDocs(collection(db, "events"));
+
+    querySnapshot.forEach((doc) => {
+      const reviewObject = {
+        id: doc.id,
+        ...doc.data()
+      }
+      console.log(reviewObject)
+      reviewArray.push(reviewObject)
+    });
+
+    reviewArray = reviewArray.sort(() => 0.5 - Math.random()).slice(0, 4);
+
+    setEvents(reviewArray)
+
+  }
+
+
+  useEffect(() => {
+
+    fetchEvents();
+
+
+
+  }, []);
+
+
+
   const images = [
     "white2.jpg",
     "traditional.jpg",
@@ -72,23 +107,101 @@ export default function Page() {
       </div>
 
 
+      <div className=' pb-2 text-[#073f3f] '>
 
-      <section className="flex flex-col items-center px-40 gap-10 py-15 mt-28"  >
 
-        <h1 className="text-5xl text-[#073f3f] max-lg:text-3xl">OUR SERVICES</h1>
+        <h2 className='text-5xl font-semibold my-[90px] text-center pt-10 '>Our Services</h2>
 
-        <div className="flex flex-col gap-10 items-center">
-          <p className="max-lg:text-sm">
-            Waju Heavens offers complete wedding planning and coordination services — from the first “yes” to the final dance. Whether it’s your bachelor's, your makeup, your venue, or your reception, we handle every detail with elegance and care.
-          </p>
+        <div className='flex justify-center px-15 ' >
+          <div className='grid gap-10 md:grid-cols-2 lg:grid-cols-4 text-center'>
+            <div className='bg-[rgba(255,255,240,0.64)] shadow-xl space-y-4  py-4 rounded-sm  flex flex-col items-center  ' >
+              <img src="eye-shadow.gif" alt="" className="w-16  " />
 
-          <Link className="bg-[#073f3f]  text-white py-3 px-8 rounded-full " href={"/services"} >View All Services
-          </Link>
 
+              <h3 className=' text-xl font-semibold'>Makeup & Styling</h3>
+
+              <p className='px-8 leading-8'>Flawless bridal makeup and styling that highlight natural beauty for the bride and bridal party.</p>
+            </div>
+
+            <div className='bg-[rgba(255,255,240,0.64)] shadow-xl space-y-4  py-4 rounded-sm  flex flex-col items-center  ' >
+              <img src="tuxedo.gif" alt="" className="w-16  " />
+
+
+              <h3 className=' text-xl font-semibold'>Groom's Preparation</h3>
+
+              <p className='px-8 leading-8'>Tailored support to ensure the groom and his men are well dressed, confident, and ready.</p>
+            </div>
+
+            <div className='bg-[rgba(255,255,240,0.64)] shadow-xl space-y-4  py-4 rounded-sm  flex flex-col items-center  ' >
+              <img src="wedding.gif" alt="" className="w-16  " />
+
+
+              <h3 className=' text-xl font-semibold'>Wedding Ceremonies</h3>
+
+              <p className='px-8 leading-8'>From scared church vows to rich traditional ceremonies, we blend faith, culture, and elegance to make your union unforgettable.</p>
+            </div>
+
+            <div className='bg-[rgba(255,255,240,0.64)] shadow-xl space-y-4  py-4 rounded-sm  flex flex-col items-center  ' >
+              <img src="wine.gif" alt="" className="w-16  " />
+
+
+              <h3 className=' text-xl font-semibold'>Reception Planning</h3>
+
+              <p className='px-8 leading-8'>Seamless event flow, decor, music, and entertainment for a joyful and memorable.</p>
+            </div>
+
+
+
+          </div>
 
         </div>
 
+
+        <div className="flex justify-center pt-20">
+          <Link className="bg-[#073f3f]  text-white py-3 px-8 rounded-full " href={"/services"} >Learn More
+          </Link>
+        </div>
+
+
+
+      </div>
+
+
+      <section>
+
+        <div className=' pb-8 text-[#073f3f] '>
+
+
+          <h2 className='text-5xl font-semibold my-[90px] text-center pt-10 '>What Our Clients Say</h2>
+
+          <div className='flex justify-center px-15 ' >
+            <div className='grid gap-10 md:grid-cols-2 lg:grid-cols-4 text-center'> {events.map((rev, i) =>(
+
+
+              <div key={i}
+               className='bg-[rgba(255,255,240,0.64)] shadow-xl space-y-4  py-4 rounded-sm  flex flex-col items-center  ' >
+                {/* <img src={rev.img} alt="U" className="w-16  " /> */}
+
+
+               
+
+                <p className='px-8 leading-8'>{rev.testimonial}</p>
+
+                <span className=" text-gray-500 text-sm font italic ">{rev.title}</span>
+              </div>
+
+             ))}
+            </div>
+
+          </div>
+
+
+         
+
+
+        </div>
       </section>
+
       <section className="flex flex-col items-center px-40 gap-10 pb-15 mt-28"  >
 
         <h1 className="text-5xl text-[#073f3f] max-lg:text-3xl">About Us</h1>
--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,18 +1,36 @@
 import React from 'react'
 import Link from 'next/link'
 import Image from 'next/image'
+import { FaFacebookF } from "react-icons/fa";
+import { FaXTwitter } from "react-icons/fa6";
+import { FaInstagram } from "react-icons/fa";
+import { FaTiktok } from "react-icons/fa6";
 
 const Footer = () => {
     return (
         <main className='bg-[#073f3f] p-10  flex flex-col gap-20  justify-between text-white'>
 
-            <Link href={"/"} className='flex gap-5 items-center' >
+            <div className='flex justify-between items-center'>
 
-              <Image src={"/waju2.png"} alt="logo" width={1000} height={1000} className="w-8 h-8" />
 
-                <p className='text-4xl  font-semibold max-lg:text-2xl'> WajuHeavens</p>
-            </Link >
 
+                <Link href={"/"} className='flex gap-5 items-center' >
+
+                    <Image src={"/waju2.png"} alt="logo" width={1000} height={1000} className="w-8 h-8" />
+
+                    <p className='text-4xl  font-semibold max-lg:text-2xl'> WajuHeavens</p>
+                </Link >
+
+
+                <div className='flex gap-5 text-[#073f3f]'> 
+                   <button className=' bg-[#fffff0] w-8 h-8  rounded-full flex items-center justify-center'><FaFacebookF  /></button> 
+                   <button  className=' bg-[#fffff0] w-8 h-8  rounded-full flex items-center justify-center'> <FaXTwitter /></button>
+                   <button  className=' bg-[#fffff0] w-8 h-8  rounded-full flex items-center justify-center'><FaInstagram /></button> 
+                   <button  className=' bg-[#fffff0] w-8 h-8  rounded-full flex items-center justify-center'> <FaTiktok /></button>
+                </div>
+
+
+            </div>
             <div className='flex flex-col md:flex-row md:items-center justify-between gap-5 '>
                 <p> &copy; WajuHeavens. All rights reserved.</p>
 
